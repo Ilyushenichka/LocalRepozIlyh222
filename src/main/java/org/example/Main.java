bb2be486dfb94b5b727e666441c58b4d5e67ffd7
Ilyushenichka <kamander2007@gmail.com>
2025-09-10 16:18:48 +0300

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
          //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(StringUtils.upperCase("Hello and welcome!"));
+            System.out.println(StringUtils.upperCase("Hello, not welcome("));
     }
 }
