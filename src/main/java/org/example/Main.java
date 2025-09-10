6ba53f40170ac0a4df83470847e03c836c636cd2
Ilyushenichka <kamander2007@gmail.com>
2025-09-10 17:00:32 +0300

    Fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,7 @@ public class Main {
     public static void main(String[] args) {
          //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println(StringUtils.upperCase("Hello and welcome!"));
-            System.out.println(StringUtils.upperCase("Hello, not welcome("));
-            System.out.println(StringUtils.lowerCase("Hello, maybe welcome, maybe no)"));
-            System.out.println(StringUtils.upperCase("Hello4times"));
+            System.out.println(StringUtils.upperCase("Hello and welcome, college!"));
+            System.out.println(StringUtils.upperCase("Hello, not welcome(kip)"));
     }
 }
